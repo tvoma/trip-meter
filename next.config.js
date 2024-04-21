5a0b0d33e1d21c310811f377f4210b15c1c7a6c5
Tyoma <hello@tyoma.co>
2024-04-21 19:14:49 +0200

    feat: add pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    scope: '/app'
+})
+
 const nextConfig = {
     output: 'export'
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
